c05c767294710baa5daa0679cfcc49af89efbc22
Johan Ingvast <ingvast@gmail.com>
2018-06-24 16:22:32 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/tpope/vim-pathogen.git
+cd autoload
+ln -s ../vim-pathogen/autoload/pathogen.vim .
+
+mkdir bundle
+cd bundle
+git clone https://github.com/kien/rainbow_parentheses.vim.git
